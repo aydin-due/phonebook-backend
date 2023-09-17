c2c6b3e2507e6cdccc1f8abf0a669841471c0172
aydin :^) <a219201085@unison.mx>
2023-09-17 16:01:02 -0700

    3.4

--- index.js
+++ index.js
@@ -44,6 +44,12 @@ app.get('/api/persons/:id', (req, res) => {
     }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    persons = persons.filter(p => p.id !== id)
+    res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
