ba5412619af6deefa17f0fa6d94940a232fd786b
aydin :^) <a219201085@unison.mx>
2023-09-17 15:54:44 -0700

    3.3

--- index.js
+++ index.js
@@ -34,6 +34,16 @@ app.get('/info', (req, res) => {
     res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(p => p.id === id)
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
