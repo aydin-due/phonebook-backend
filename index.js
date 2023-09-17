ac04a4010d67e3cf699101e0eab32869a21a724d
aydin :^) <a219201085@unison.mx>
2023-09-17 15:47:24 -0700

    3.2

--- index.js
+++ index.js
@@ -29,6 +29,11 @@ app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/info', (req, res) => {
+    const date = new Date()
+    res.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
