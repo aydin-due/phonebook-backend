e21113be1fdab9e5ab9f7d664c568736de9ffde9
aydin :^) <a219201085@unison.mx>
2023-09-17 18:12:44 -0700

    3.8

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 app.use(express.json())
-app.use(morgan('tiny'))
+
+morgan.token('req-body', (req, res) => {
+    return JSON.stringify(req.body)
+})
+
+app.use(
+    morgan(':method :url :status :res[content-length] - :response-time ms :req-body')
+)
+
 
 let persons = [
     { 
