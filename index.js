efc08d2f4bf747b5f1a654e49df0c83bfa4fdaad
aydin :^) <a219201085@unison.mx>
2023-09-18 12:09:32 -0700

    3.10

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
