95d477c8bafbd27f38816a617372bac074974c16
aydin :^) <a219201085@unison.mx>
2023-09-18 12:14:43 -0700

    3.11

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 app.use(express.json())
 app.use(cors())
+app.use(express.static('dist'))
 
 morgan.token('req-body', (req, res) => {
     return JSON.stringify(req.body)
